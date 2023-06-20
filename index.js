a62f4835342e45cbb3749e5d4d92aa17c5033c5f
cyrille <cyrilllerotich@gmail.com>
2023-06-20 08:00:39 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block){
+    return Math.abs(block-42)
+}
+function   distanceFromHqInFeet(block){
+    return distanceFromHqInBlocks(block)*264;
+}
+function  distanceTravelledInFeet(startblock,endblock){
+    return Math.abs(startblock-endblock)*264
+}
+function calculatesFarePrice(distance){
+   if(distance < 400){
+    return `free`
+   }
+   else if(distance >400 && distance <= 2000){
+    return (distance-400)*0.02;
+   } 
+   else if(distance>2000 && distance <=2500){
+    return 25;
+   }
+   else if(distance> 2500){
+   return `does not allow rides over 2500 feet`;
+   }
+}
+
+
